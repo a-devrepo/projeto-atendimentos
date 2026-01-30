e641547bd36bf269bc56c92ad82b8bd1e606fc1c
alison <adevpr11@gmail.com>
2026-01-30 11:30:38 -0300

    refactor(script): altera tipo de dado da coluna id

--- src/main/java/sql/script.sql
+++ src/main/java/sql/script.sql
@@ -1,6 +1,6 @@
 CREATE TABLE atendimentos
 (
-    id       SERIAL PRIMARY KEY,
+    id       UUID PRIMARY KEY,
     usuario  VARCHAR(50)   NOT NULL,
     pergunta VARCHAR(500)  NOT NULL,
     resposta VARCHAR(2000) NOT NULL,
